3f5ba7b16aad67af7c0dcda68c992cbe07c7b62c
artur <arturaniki@gmail.com>
2024-12-17 00:59:06 +0200

    commit 2

--- app/src/main/java/com/example/ap/AddItemFragment.kt
+++ app/src/main/java/com/example/ap/AddItemFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.ap.databinding.AddRecipeBinding
@@ -23,6 +24,16 @@ class AddItemFragment : Fragment() {
         _binding=AddRecipeBinding.inflate(inflater,container,false)
         //מהוספת פריט בחזרה לרשימה ראשית
         binding.btnBack.setOnClickListener(){
+            findNavController().navigate(R.id.action_addItemFragment_to_allItemsFragment)
+        }
+
+        binding.btnAddFood.setOnClickListener(){
+            val bundle = bundleOf(
+                "title" to binding.foodNameInput.text.toString(),           // שם האוכל
+                "author" to binding.authorNameInput.text.toString(),       // שם המחבר
+                "description" to binding.foodDescriptionInput.text.toString(), // תיאור
+                "ingredients" to binding.ingredientsDescriptionInput.text.toString() // רכיבים
+            )
             findNavController().navigate(R.id.action_addItemFragment_to_allItemsFragment)
         }
 
